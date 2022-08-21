7c86ee38ab0e0f3a0c40613a3c97d0dd6ec24e81
SamarthDambalkar7 <Samarthdambalkar7@gmail.com>
2022-08-21 12:37:31 +0530

    implemented clipboard get function

--- lib/pages/CopyServicePage.dart
+++ lib/pages/CopyServicePage.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 import 'package:clipboard/clipboard.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
@@ -9,6 +10,25 @@ class CopyServicePage extends StatefulWidget {
 }
 
 class _CopyServicePageState extends State<CopyServicePage> {
+  @override
+  void initState() {
+    getClipboardData();
+    super.initState();
+  }
+
+  //variable declarations.
+  List<String> clipboardArray = [];
+
+  void getClipboardData() async {
+    ClipboardData? cData = await Clipboard.getData(Clipboard.kTextPlain);
+    String? copiedData = cData!.text;
+    print(copiedData);
+
+    setState(() {
+      clipboardArray.add(copiedData.toString());
+    });
+  }
+
   @override
   Widget build(BuildContext context) => Scaffold(
         appBar: AppBar(
@@ -37,16 +57,40 @@ class _CopyServicePageState extends State<CopyServicePage> {
                 SizedBox(
                   height: 30,
                 ),
-
                 CupertinoButton.filled(
-                    child: Text(Clipboard.kTextPlain),
-                    onPressed: () async {
-                      ClipboardData cdata =
-                          await Clipboard.getData(Clipboard.kTextPlain);
-                      // String copiedtext = cdata.text.toString();
-                      print(copiedtext);
+                    child: Text('click'),
+                    onPressed: () {
+                      showDialog(
+                          context: context,
+                          builder: (BuildContext context) => SimpleDialog(
+                                children: [
+                                  Align(
+                                    alignment: Alignment.center,
+                                    child: ListView.builder(
+                                        itemBuilder: (context, index) =>
+                                            ListTile(
+                                              title: Text(
+                                                  '• ' + clipboardArray[index]),
+                                            )),
+                                  ),
+                                ],
+                              ));
+                      getClipboardData();
                     }),
-                // Text(Clipboard.kTextPlain),
+                SizedBox(
+                  height: 30,
+                ),
+                Container(
+                  color: Colors.blueAccent,
+                  height: 300,
+                  child: Flexible(
+                      child: ListView.builder(
+                          shrinkWrap: true,
+                          itemCount: clipboardArray.length,
+                          itemBuilder: ((context, index) => ListTile(
+                                title: Text('• ' + clipboardArray[index]),
+                              )))),
+                )
               ],
             ),
           ),
